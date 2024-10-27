975d1e8c00661a44fb227119873f862905a1c190
gitlab124 <dsf78er2xder512325er@tutamail.com>
2024-10-27 18:40:29 +0100

    run.ps1

---
+++ run.ps1
@@ -0,0 +1,13 @@
+# Chemin complet vers WINKey.vbs dans le dossier WIDFHost de ProgramData
+$chemin_executable = Join-Path $env:ProgramData 'WIDFHost\WINKey.vbs'
+
+# Attendre 30 secondes pour s'assurer que l'application est présente
+Start-Sleep -Seconds 30
+
+# Vérifie si le fichier existe et exécute-le
+if (Test-Path $chemin_executable) {
+    # Exécuter le fichier VBS en utilisant cscript
+    & 'C:\Windows\System32\cscript.exe' $chemin_executable
+} else {
+    Write-Host "Le script WINKey.vbs est introuvable dans le dossier WIDFHost de ProgramData."
+}
